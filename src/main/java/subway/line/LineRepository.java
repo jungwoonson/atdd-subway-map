fd5dc1cea18efbe66a0092831f3b5f80db68532d
jungwoonson <jungwoon727@gmail.com>
2024-07-06 18:15:44 +0900

    refactor: LineRepository import 정리

--- src/main/java/subway/line/LineRepository.java
+++ src/main/java/subway/line/LineRepository.java
@@ -2,8 +2,6 @@ package subway.line;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Arrays;
-
 public interface LineRepository extends JpaRepository<Line, Long> {
     Line findOneById(Long id);
 }
