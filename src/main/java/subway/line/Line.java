acd290b4f2bb9f65c278d4b038e011ddb2bc1cd5
jungwoonson <jungwoon727@gmail.com>
2024-07-06 17:08:23 +0900

    feat: 노선 생성 API 추가

---
+++ src/main/java/subway/line/Line.java
@@ -0,0 +1,100 @@
+package subway.line;
+
+import javax.persistence.*;
+
+@Entity
+public class Line {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(length = 20, nullable = false)
+    private String name;
+    @Column(length = 20, nullable = false)
+    private String color;
+    @Column(nullable = false)
+    private Long upStationId;
+    @Column(nullable = false)
+    private Long downStationId;
+    @Column(nullable = false)
+    private Integer distance;
+
+    public Line() {
+    }
+
+    private Line(Builder builder) {
+        this.id = builder.id;
+        this.name = builder.name;
+        this.color = builder.color;
+        this.upStationId = builder.upStationId;
+        this.downStationId = builder.downStationId;
+        this.distance = builder.distance;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public Long getUpStationId() {
+        return upStationId;
+    }
+
+    public Long getDownStationId() {
+        return downStationId;
+    }
+
+    public Integer getDistance() {
+        return distance;
+    }
+
+    public static class Builder {
+        private Long id;
+        private String name;
+        private String color;
+        private Long upStationId;
+        private Long downStationId;
+        private Integer distance;
+
+        public Builder id(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder color(String color) {
+            this.color = color;
+            return this;
+        }
+
+        public Builder upStationId(Long upStationId) {
+            this.upStationId = upStationId;
+            return this;
+        }
+
+        public Builder downStationId(Long downStationId) {
+            this.downStationId = downStationId;
+            return this;
+        }
+
+        public Builder distance(Integer distance) {
+            this.distance = distance;
+            return this;
+        }
+
+        public Line build() {
+            return new Line(this);
+        }
+    }
+}
---
+++ src/main/java/subway/line/LineService.java
@@ -0,0 +1,64 @@
+package subway.line;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import subway.StationResponse;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+public class LineService {
+
+    private LineRepository lineRepository;
+
+    public LineService(LineRepository lineRepository) {
+        this.lineRepository = lineRepository;
+    }
+
+    @Transactional
+    public LineResponse lineSave(LineRequest lineRequest) {
+        Line line = lineRepository.save(createLine(lineRequest));
+        return createLineResponse(line);
+    }
+
+    private Line createLine(LineRequest lineRequest) {
+        return new Line.Builder()
+                .name(lineRequest.getName())
+                .color(lineRequest.getColor())
+                .upStationId(lineRequest.getUpStationId())
+                .downStationId(lineRequest.getDownStationId())
+                .distance(lineRequest.getDistance())
+                .build();
+    }
+
+    private LineResponse createLineResponse(Line line) {
+        return new LineResponse.Builder()
+                .id(line.getId())
+                .color(line.getColor())
+                .stations(createStations(line.getUpStationId(), line.getDownStationId()))
+                .build();
+    }
+
+    private List<StationResponse> createStations(Long upStationId, Long downStationId) {
+        return List.of(
+                createStation(upStationId),
+                createStation(downStationId)
+        );
+    }
+
+    private StationResponse createStation(Long stationId) {
+        return new StationResponse(stationId, findStationNameById(stationId));
+    }
+
+    private String findStationNameById(Long stationId) {
+        switch (stationId.intValue()) {
+            case 1:
+                return "지하철역";
+            case 2:
+                return "새로운지하철역";
+            default:
+                return "또다른지하철역";
+        }
+    }
+}
