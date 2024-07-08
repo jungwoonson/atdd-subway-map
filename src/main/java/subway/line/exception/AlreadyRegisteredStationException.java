32c52abff089acc0c5631bea78099f455db0e5b3
jungwoonson <jungwoon727@gmail.com>
2024-07-08 23:12:35 +0900

    feat: 이미 해당 노선에 등록되어 있는 역을 새로운 구간의 하행역으로 등록하려 하면 400 응답

--- src/main/java/subway/line/exception/AlreadyRegisteredStationException.java
+++ src/main/java/subway/line/exception/AlreadyRegisteredStationException.java
@@ -2,7 +2,7 @@ package subway.line.exception;
 
 public class AlreadyRegisteredStationException extends IllegalArgumentException {
 
-    private static final String message = "해당 노선에 등록되지 않은 하행역을 등록해 주세요.";
+    private static final String message = "해당 노선에 등록되지 않은 지하철 역을 하행역으로 등록해 주세요.";
 
     public AlreadyRegisteredStationException() {
         super(message);
